dd81a6d3cacea50d6b27bdf4ee7c3f333ff7d1a8
nanoska <nlurbe.trompeta@gmail.com>
2024-11-04 15:53:15 -0300

    fix url commented single class

--- web/urls.py
+++ web/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     
     path('contacto/', ContactView.as_view(), name='contact'),
     path('under-construction/', UnderConstructionView.as_view(), name='under_construction'),
-    path('clases-particulares/', SingleLessonsView.as_view(), name='single_lessons'),
+    # path('clases-particulares/', SingleLessonsView.as_view(), name='single_lessons'),
     path('sobre-nosotros/', AboutUsView.as_view(), name='about_us'),
 ]
